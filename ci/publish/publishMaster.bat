82ed671ebc8014bcb70b59c5c4c6839b832aa817
Berislav <berislav.hunjadi@gmail.com>
2018-06-28 11:01:16 +0200

    Version bump

--- ci/publish/publishMaster.bat
+++ ci/publish/publishMaster.bat
@@ -1,6 +1,6 @@
 CALL npm run build
 CALL node .\ci\publish\replacePackageEntry react-data-grid true
-for /f %%i in ('node .\ci\publish\getReleaseVersion') do set RELEASE_VERSION=%%i
+for /f %%i in ('node .\ci\publish\getNextVersion %1') do set RELEASE_VERSION=%%i
 CALL npm run beforepublish
 CALL lerna publish --repo-version %RELEASE_VERSION% --skip-git  --yes
 CALL node .\ci\publish\replacePackageEntry react-data-grid
